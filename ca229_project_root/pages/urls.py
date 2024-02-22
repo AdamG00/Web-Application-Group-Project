09d6a42c745e17721fae9248a453211836451dcf
Adam Gilroy <gilroya4@l125-39.computing.dcu.ie>
2024-02-22 19:55:34 +0000

    Website set up week 2

--- ca229_project_root/pages/urls.py
+++ ca229_project_root/pages/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('magic', views.magic, name = 'magic'),
+    path('signup', views.signup, name = 'signup'),
+    path('login', views.login, name = 'login'),
+    path('about', views.about, name = 'about'),
     path('', views.index, name='index'),
 ]
--- ca229_project_root/pages/views.py
+++ ca229_project_root/pages/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . models import Page
 
 def index(request):
-    return HttpResponse("<h1>Hello and welcome to FitChallenge<h1>")
+    pg = Page.objects.get(permalink='/')
+    return HttpResponse(pg.bodytext)
+
+def about(request):
+    pg = Page.objects.get(permalink='/')
+    return HttpResponse(pg.bodytext)
+
+def login(request):
+    pg = Page.objects.get(permalink='/')
+    return HttpResponse(pg.bodytext)
+
+def signup(request):
+    pg = Page.objects.get(permalink='/')
+    return HttpResponse(pg.bodytext)
+
+def magic(request):
+    pg = Page.objects.get(permalink='/')
+    return HttpResponse(pg.bodytext)
 
 # Create your views here.

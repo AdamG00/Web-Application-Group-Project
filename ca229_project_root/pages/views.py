560a80e771c12ba954ba7c646e9ea0f0febc3797
Adam Gilroy <gilroya4@l125-39.computing.dcu.ie>
2024-02-24 18:19:42 +0000

    Week 3 fit challenge logo added

--- ca229_project_root/pages/views.py
+++ ca229_project_root/pages/views.py
@@ -4,22 +4,42 @@ from . models import Page
 
 def index(request):
     pg = Page.objects.get(permalink='/')
-    return HttpResponse(pg.bodytext)
+    context = {
+	'title': pg.title,
+	'content': pg.bodytext
+    }
+    return render(request, 'base.html', context)
 
 def about(request):
-    pg = Page.objects.get(permalink='/')
-    return HttpResponse(pg.bodytext)
+    pg = Page.objects.get(permalink='/about')
+    context = {
+	'title': pg.title,
+	'content': pg.bodytext
+    }
+    return render(request, 'base.html', context)
 
 def login(request):
-    pg = Page.objects.get(permalink='/')
-    return HttpResponse(pg.bodytext)
+    pg = Page.objects.get(permalink='/login')
+    context = {
+	'title': pg.title,
+	'content': pg.bodytext
+    }
+    return render(request, 'base.html', context)
 
 def signup(request):
-    pg = Page.objects.get(permalink='/')
-    return HttpResponse(pg.bodytext)
+    pg = Page.objects.get(permalink='/signup')
+    context = {
+	'title': pg.title,
+	'content': pg.bodytext
+    }
+    return render(request, 'base.html', context)
 
 def magic(request):
-    pg = Page.objects.get(permalink='/')
-    return HttpResponse(pg.bodytext)
+    pg = Page.objects.get(permalink='/magic')
+    context = {
+	'title': pg.title,
+	'content': pg.bodytext
+    }
+    return render(request, 'base.html', context)
 
 # Create your views here.

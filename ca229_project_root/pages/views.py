be8a64d692487b631447a60b2bac046807f637a0
gilroya4 <adam.gilroy4@mail.dcu.ie>
2024-02-27 00:05:43 +0000

    templates for each page set up feel free to make a custom css file for your specific page if you need to add features for your page alone

--- ca229_project_root/pages/views.py
+++ ca229_project_root/pages/views.py
@@ -2,15 +2,52 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from . models import Page
 
-def index(request, pagename=''):
-    pagename = '/' + pagename
-    pg = Page.objects.get(permalink=pagename)
+def index(request):
+    pg = Page.objects.get(permalink='/')
     context = {
 		'title': pg.title,
-		'content': pg.bodytext, # note the end-of-line comma
+		'content': pg.bodytext,
 		'last_updated': pg.update_date,
 		'page_list': Page.objects.all(),
 	}
-    return render(request, 'pages/page.html', context)
+    return render(request, 'pages/indexpage.html', context)
 
-# Create your views here.
+def about(request):
+    pg = Page.objects.get(permalink='/about')
+    context = {
+		'title': pg.title,
+		'content': pg.bodytext,
+		'last_updated': pg.update_date,
+		'page_list': Page.objects.all(),
+	}
+    return render(request, 'pages/aboutpage.html', context)
+
+def login(request):
+    pg = Page.objects.get(permalink='/login')
+    context = {
+		'title': pg.title,
+		'content': pg.bodytext,
+		'last_updated': pg.update_date,
+		'page_list': Page.objects.all(),
+	}
+    return render(request, 'pages/loginpage.html', context)
+
+def signup(request):
+    pg = Page.objects.get(permalink='/signup')
+    context = {
+		'title': pg.title,
+		'content': pg.bodytext,
+		'last_updated': pg.update_date,
+		'page_list': Page.objects.all(),
+	}
+    return render(request, 'pages/signuppage.html', context)
+
+def magic(request):
+    pg = Page.objects.get(permalink='/magic')
+    context = {
+		'title': pg.title,
+		'content': pg.bodytext,
+		'last_updated': pg.update_date,
+		'page_list': Page.objects.all(),
+	}
+    return render(request, 'pages/magicpage.html', context)
